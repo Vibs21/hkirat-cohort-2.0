689e51abb7b13a07fbda77bb32eb2f1bf70ff0fb
Vaibhav Bajpayee <vaibhavbajpayee2109@gmail.com>
2023-12-07 18:07:22 +0530

    easy solved 1 and 2

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,25 @@
 */
 
 function isAnagram(str1, str2) {
-
+  str2 = str2.toLowerCase();
+  str1 = str1.toLowerCase();
+  let val = false;
+  if(str1.length == str2.length) {
+    for (let i=0;i<str1.length;i++) {
+      val = str2.includes(str1[i]) ? true : false;
+      if (!val) {
+        return false;
+      }
+    } 
+    return true;
+  } else {
+    return false;
+  }
 }
 
+console.log('text is Anagram:', isAnagram('spar', 'sptr'));
+// another way to do is, 
+// 1 make all the text into lower character
+// 2 sort all the letters from both the srring
+// now a == b, it should be working
 module.exports = isAnagram;
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -6,7 +6,56 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  // let a = transactions
+  //             .map((val)=> {return val.category})
+  //             .filter((val, index, arr) => {
+  //               return arr.indexOf(val) == index;
+  //             })
+
+  let categorySum = transactions.reduce((accumulator, curVal) => {
+     const {category, price} = curVal;
+    
+     if(!accumulator[category]) {
+        accumulator[category] = 0;
+     }
+
+        accumulator[category] += price;
+        
+        return accumulator;
+  }, {})
+
+  const result = Object.keys(categorySum)
+                        .map(category => ({
+                          category,
+                          totalSpent: categorySum[category]
+                        }))
+  
+  console.log(result)
+  return result;
 }
+
+// calculateTotalSpentByCategory([                                                                                                              
+//   {
+//     id: 1,
+//     timestamp: 1656076800000,
+//     price: 10,
+//     category: 'Food',
+//     itemName: 'Pizza'
+//   },
+//   {
+//     id: 2,
+//     timestamp: 1656105600000,
+//     price: 20,
+//     category: 'Food',
+//     itemName: 'Burger'
+//   },
+//   {
+//     id: 3,
+//     timestamp: 1656134400000,
+//     price: 30,
+//     category: 'Cloth',
+//     itemName: 'Sushi'
+//   }
+// ])
 
 module.exports = calculateTotalSpentByCategory;
