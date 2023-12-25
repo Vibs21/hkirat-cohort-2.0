8247d1cafc63ab9744da3107986cd48a200c5a5a
Vaibhav Bajpayee <vaibhavbajpayee2109@gmail.com>
2023-12-25 12:45:13 +0530

    body parser added and tried to get the data on the body of the req object

--- PlayGround/week2/http-server/express.js
+++ PlayGround/week2/http-server/express.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     console.log("Someone is trying to access the homepage");
+    console.log("body", req.body);
     res.send('Hello Dostoo');
 });
 
 
 app.post('/postData', function(req, res) {
-    console.log(req.body);
+    console.log("bodyy", req.body);
     res.send('done');
 })
 
