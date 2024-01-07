b9cba38858299b6757a1692495067e8ffbd7553d
Vaibhav Bajpayee <vaibhavbajpayee2109@gmail.com>
2024-01-08 00:27:17 +0530

    error middleware created to send the error to the client and catch it

--- PlayGround/week2/http-server/express.js
+++ PlayGround/week2/http-server/express.js
@@ -26,4 +26,17 @@ app.post('/queryData', function(req, res) {
     res.send('done');
 })
 
+app.get('/error', function(req,res,next) {
+    console.log('error route');
+    const err = new Error('This is an intentional error');
+    next(err);
+})
+
+const errorHandler = (err,req,res,next) => {
+    console.log('err', err.stack);
+    res.status(500).send('Something went wrong!')
+}
+
+app.use(errorHandler);
+
 app.listen(3000);
