48ddcb2009aed38a5f071db78b909b7d645c7dc3
Vaibhav Bajpayee <vaibhavbajpayee2109@gmail.com>
2024-05-12 17:21:50 +0530

    - library mongoose added - added code in the node using express to run in port 3000

--- PlayGround/week8/paytm-main/backend/index.js
+++ PlayGround/week8/paytm-main/backend/index.js
@@ -1,3 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/', (req,res) =>{
+    res.send('Hello World!');
+})
+
+app.listen(3000);
