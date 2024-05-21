5f9003dd25f9687adbebcf2f30138390afbe0188
Vaibhav Bajpayee <vaibhavbajpayee2109@gmail.com>
2024-05-21 20:00:32 +0530

    implemented transactions in the DB for paytm bank to secure the transactions in case of failure in between

--- PlayGround/week8/paytm-main/backend/routes/account.js
+++ PlayGround/week8/paytm-main/backend/routes/account.js
@@ -1,13 +1,9 @@
 const { Router } = require('express');
 const isUserAthenticated = require('../middleware/userAuthMiddleware');
 const { User, Account } = require('../db/db');
+const { default: mongoose } = require('mongoose');
 
 const router = Router();
-
-// from, to, amount, transaction (Session) 
-router.get('/account/transfer', (req, res) => {
-    res.send({message: 'money creadit sccessfully'});
-})
 
 router.get('/account/balance', isUserAthenticated, async (req, res) => {
     let accountBal = await Account.findOne({
@@ -15,6 +11,57 @@ router.get('/account/balance', isUserAthenticated, async (req, res) => {
     }).select('balance _id');
 
     res.send({balance: accountBal.balance});
+})
+
+// from, to, amount, transaction (Session) 
+// check if user is having sufficient balance, if benef user exists 
+router.post('/account/transfer', isUserAthenticated, async (req, res) => {
+    const session = await mongoose.startSession();
+
+    session.startTransaction();
+    
+    const { to, amount } = req.body;
+
+    //checking if user is having sufficiemt balance or not
+    const user = await Account.findOne({userId: req.userId}).session(session);
+
+    
+    if(user.balance < amount) {
+        await session.abortTransaction();
+        return res.status(400).send({message: 'Insufficient Balance'});
+    }
+    
+    const toAccount = await Account.findOne({ userId: to }).session(session);
+    
+
+    if (!toAccount) {
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Invalid account"
+        });
+    }
+
+    //debit money from User account
+    await Account.updateOne({userId: req.userId},{
+        $inc: {'balance': -amount}
+    }).session(session);
+
+    console.log('user', toAccount);
+
+    
+    // //credit money to the benef user
+    await Account.updateOne({
+        userId: to
+    },{
+        $inc: {'balance': amount}
+    }).session(session);
+
+    
+// Commit the transaction
+    await session.commitTransaction();
+    res.json({
+    message: "Transfer successful"
+});
 })
 
 module.exports = router;
