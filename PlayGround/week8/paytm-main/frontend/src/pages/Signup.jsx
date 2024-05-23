dda23b12786fc70b8dc3df8f08a2b226b36bef70
Vaibhav Bajpayee <vaibhavbajpayee2109@gmail.com>
2024-05-23 22:44:29 +0530

    signup page completed by integrating axios call

--- PlayGround/week8/paytm-main/frontend/src/pages/Signup.jsx
+++ PlayGround/week8/paytm-main/frontend/src/pages/Signup.jsx
@@ -2,12 +2,28 @@ import { useState } from 'react';
 import { Heading } from '../components/Heading';
 import { InputBox } from '../components/InputBox';
 import { SubHeading } from '../components/SubHeader';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function SignUp() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+
+  const navigate = useNavigate();
+
+  const userSignup = async () => {
+    const response = await axios.post("http://localhost:3000/api/v1/user/signup", {
+        firstName,
+        lastName,
+        email,
+        password
+    });
+    localStorage.setItem("token", response.data.token)
+    navigate("/dashboard")
+  }
 
   return (
     <div className='flex justify-center items-center h-[85vh]'>
@@ -24,7 +40,7 @@ function SignUp() {
           </div>
           <div className="flex items-center justify-between flex-col">
             <button className="bg-slate-950 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-[100%] mb-2"
-              type="button">
+              type="button" onClick={userSignup}>
               Sign Up
             </button>
             <div>
