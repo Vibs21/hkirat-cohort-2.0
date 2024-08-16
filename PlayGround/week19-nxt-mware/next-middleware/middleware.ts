de0ebc702a092ebcf40bb250ca23f816bb88b7c9
Vaibhav Bajpayee <vaibhavbajpayee2109@gmail.com>
2024-08-16 19:27:36 +0530

    - using config, given by next js, you can run the middleware for the selective routes - by using the 'request.nextUrl.pathname.startsWith('admin')' inside the if condtion, you can customise few things for specific route

--- PlayGround/week19-nxt-mware/next-middleware/middleware.ts
+++ PlayGround/week19-nxt-mware/next-middleware/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 
 let requestCount = 0;
-export function middleware(req: NextRequest) {
+export function middleware1(req: NextRequest) {
     requestCount++;
     console.log('number of request is: ', requestCount);
     const response = NextResponse.next();
@@ -11,3 +11,22 @@ export function middleware(req: NextRequest) {
 
     return response
 }
+
+//NOTE: Now it will only run when the route gets matched with the path
+// export const config = {
+//     matcher: '/api/:path*',
+// }
+
+// NOTE: this is path matched, if you want to do something for the specific route, then you can match the path and do the logic thing
+export function middleware(request: NextRequest) {
+    console.log(request.nextUrl.pathname)
+    if (request.nextUrl.pathname.startsWith('/admin')) {
+      return NextResponse.redirect(new URL('/signin', request.url))
+    }
+   
+    if (request.nextUrl.pathname.startsWith('/dashboard')) {
+      return NextResponse.next()
+    }
+}
+
+
---
+++ PlayGround/week19-nxt-mware/note.md
@@ -0,0 +1,8 @@
+
+
+*Middleware In NextJs*
+
+- to use middleware, add the file middleware.ts at the root of the folder
+- this middleware is applicable for all the routes
+  - there are is way though to make it work for specific route
+- 
